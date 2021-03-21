0ccbbc3c74a2a2bbab46cd8e140d394b0ac3eb15
christian <chrisgabor8@gmail.com>
2021-03-21 09:07:19 -0700

    added plotting

--- plot_scores.py
+++ plot_scores.py
@@ -52,28 +52,59 @@ def plot_loss(scores, name, save_path=None):
     plt.show()
 
 
+def find_test_val_accs(folder_names):
+    pass
+
+
+
 if __name__ == "__main__":
 
     # Change names of folders to what you have locally
     # folder : title
-    saved_data = {
-        'ResPostMP_4_layer_128_dim': 'Residual Post Message 4 Layers 128 Hidden Dims Batch Size 4',
-        'GraphSage_baseline': 'GraphSage with 3 Layers 256 Hidden Dims Batch Size 4',
-        'ResPostMP_6_layer_128_dim_32_batch': 'Residual Post Message 6 Layers'
-    }
-
+    # saved_data = {
+    #     'ResPostMP_4_layer_128_dim': 'Residual Post Message 4 Layers 128 Hidden Dims Batch Size 4',
+    #     'GraphSage_baseline': 'GraphSage with 3 Layers 256 Hidden Dims Batch Size 4',
+    #     '6_layer_2': 'Residual Post Message 6 Layers',
+    # }
     # Shows accuracy curves
-    for folder in saved_data.keys():
-        scores, args = get_scores(folder)
-        plot_train_val_test(scores, name=saved_data[folder], save_path=None)
-        print(args)
-        print("")
+    # for folder in saved_data.keys():
+    #     scores, args = get_scores(folder)
+    #     # plot_train_val_test(scores, name=saved_data[folder], save_path=None)
+    #     print(folder)
+    #     print(args)
+    #     print("")
 
     # Plots the test accs for different layer depths
     layer_diffs = {
-        'ResPostMP_4_layer_128_dim': '4 Layers Batch 4',
-        'ResPostMP_6_layer_128_dim_32_batch': '6 layers Batch 32'
+        '4_layer': '4 layers',
+        '5_layer': '5 layers',
+        '6_layer_1': '6 layers',
+        '10_layer': '10 layers',
     }
+
+    # plot test, val, loss of layer diffs
     plot_layer_diffs(layer_diffs, title='Test Accuracy vs Layer Depth', key='test', save_path=None)
+    plot_layer_diffs(layer_diffs, title='Val Accuracy vs Layer Depth', key='valid', save_path=None)
+    plot_layer_diffs(layer_diffs, title='Loss vs Layer Depth', key='loss', save_path=None)
+
+    six_layers = {
+        '6_layer_1': '6 layers',
+        '6_layer_2': '6 layers',
+        '6_layer_3': '6 layers',
+        '6_layer_4': '6 layers',
+        '6_layer_5': '6 layers',
+    }
+    plot_layer_diffs(six_layers, title='Acc across runs', key='test', save_path=None)
+
+    other_models = {
+        'GraphSage_baseline': 'GraphSage Linear Head',
+        'no_mp_4': 'Residual GCN',
+    }
+
+    sum_vs_mean = {
+        '6_layer_1': '6 layers',
+        'postMP_sum_agg': '6 layers',
+    }
+
 
     # Add any extra combinations of folders to plot on the same graph if needed
